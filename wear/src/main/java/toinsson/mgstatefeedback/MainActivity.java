2ac2e36eca0aadd010e227cff0454f874a2cddc7
toinsson <antoine.loriette@gmail.com>
2016-11-03 10:27:42 +0000

    add images for watch

--- wear/src/main/java/toinsson/mgstatefeedback/MainActivity.java
+++ wear/src/main/java/toinsson/mgstatefeedback/MainActivity.java
@@ -4,18 +4,23 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.wearable.activity.WearableActivity;
 import android.support.wearable.view.BoxInsetLayout;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+
+import static toinsson.mgstatefeedback.R.drawable.tick;
 
 public class MainActivity extends WearableActivity {
 
@@ -27,7 +32,7 @@ public class MainActivity extends WearableActivity {
     private RelativeLayout mContainerView;
     private TextView mTextView;
     private TextView mClockView;
-
+    private ImageView mImageView;
     private BroadcastReceiver receiver;
 
     @Override
@@ -37,14 +42,31 @@ public class MainActivity extends WearableActivity {
         setAmbientEnabled();
 
         mContainerView = (RelativeLayout) findViewById(R.id.container);
-        mTextView = (TextView) findViewById(R.id.sensor_1);
+        mTextView = (TextView) findViewById(R.id.state);
         mClockView = (TextView) findViewById(R.id.clock);
+        mImageView = (ImageView) findViewById(R.id.imageView);
 
         receiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
                 String s = intent.getStringExtra(DataLayerListenerService.SEND_STATE_DATA);
-                Log.d(TAG, "received broadcast from service");
+                Log.d(TAG, "received broadcast from service " + s);
+
+                if (s.contains("grasp")) {
+                    Resources res = getResources();
+                    Drawable drawable = res.getDrawable(R.drawable.tick);
+                    mImageView.setBackground(drawable);
+                }
+                else if (s.contains("release")) {
+                    Resources res = getResources();
+                    Drawable drawable = res.getDrawable(R.drawable.cross);
+                    mImageView.setBackground(drawable);
+                }
+                else if (s.contains("disabled")) {
+                    Resources res = getResources();
+                    Drawable drawable = res.getDrawable(R.drawable.speak);
+                    mImageView.setBackground(drawable);
+                }
                 mTextView.setText(s);
             }
         };
